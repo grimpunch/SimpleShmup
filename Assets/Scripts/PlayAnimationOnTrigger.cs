e0c41f810ae343718fd247d497daf2e297fb2d4f
Christian Murphy <christheromguy@googlemail.com>
2015-05-06 13:44:09 +0100

    Animation based spawn movement and wave composition, may refactor later

---
+++ Assets/Scripts/PlayAnimationOnTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAnimationOnTrigger : MonoBehaviour {
+    public string name = "GamePlayArea";
+    public Animator animator;
+    public float delay = 0.0f;
+    
+    private Transform newParent;
+
+    public void Start() {
+        if (animator == null) {
+            animator = gameObject.GetComponent<Animator>();
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D col2D) {
+        Invoke("AnimateNow", delay);
+    }
+
+    void AnimateNow() {
+        animator.enabled = true;
+        gameObject.GetComponent<ParentOnTrigger>().enabled = false;
+    }
+}
