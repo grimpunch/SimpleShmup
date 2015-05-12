190e38e122cf1727063f879b64eabba475aaa4d3
Christian Murphy <christheromguy@googlemail.com>
2015-05-12 22:57:12 +0100

    Actually fixed boss animations and spawning now. there was an issue where motion of the stage was not being applied to the object, solved by encapsulating animated game object in an empty game object

---
+++ Assets/Scripts/BossSpawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossSpawn : MonoBehaviour {
+
+    public GameObject spawnPrefab;
+    
+    private bool spawned;
+    
+    // Use this for initialization
+    void Start() {
+        Spawn();
+        if (spawned) { Destroy(gameObject); }
+    }
+
+    void Spawn() {
+        GameObject spawnGO = (GameObject)Instantiate(spawnPrefab, transform.position, transform.rotation);
+        spawnGO.name = spawnPrefab.name;
+        spawned = true;
+    }
+}
