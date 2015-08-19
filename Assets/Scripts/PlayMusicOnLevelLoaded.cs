a0fd448e07470cf35fdc6b60b16f772a636939c3
Christian Murphy <christheromguy@googlemail.com>
2015-08-19 20:53:32 +0100

    Music pauses and unpauses along with game state. Music does not play until level is fully loaded

---
+++ Assets/Scripts/PlayMusicOnLevelLoaded.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMusicOnLevelLoaded : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        if (Application.isPlaying) gameObject.GetComponent<AudioSource>().Play(); 
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Application.isPlaying && Utils.Paused) gameObject.GetComponent<AudioSource>().Pause();
+        if (Application.isPlaying && !Utils.Paused) gameObject.GetComponent<AudioSource>().UnPause();
+    }
+}
