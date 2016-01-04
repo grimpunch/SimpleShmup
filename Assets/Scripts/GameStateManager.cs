e703fc7d04f1d059647a993bb14e0fa801a416cf
Christian Murphy <christheromguy@googlemail.com>
2016-01-04 21:40:44 +0000

    Implemented Async loading main menu into level
    Game State management tying levels together to follow.

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateManager : MonoBehaviour {
+	public static GameStateManager GameStateManagerInstance;
+	
+	// Use this for initialization
+	void Start () {
+		if (GameStateManagerInstance != null)
+		{
+			GameObject.Destroy(gameObject);
+		}
+		else
+		{
+			GameObject.DontDestroyOnLoad(gameObject);
+			GameStateManagerInstance = this;
+		}
+	}
+
+	public void NewGame(){
+		SceneManager sceneManager = GameObject.Find("SceneManager").GetComponent<SceneManager>();
+		sceneManager.StartCoroutine("LoadScene","Level1");
+		Debug.Log("New Game Clicked");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+	public static SceneManager SceneManagerInstance;
+	public GameObject loadingUI;
+	private AsyncOperation async;
+	private float timeToWaitAroundLoads = 2f;
+	private float timeWaited;
+	// Use this for initialization
+	void Start () {
+		if (SceneManagerInstance != null)
+		{
+			GameObject.Destroy(gameObject);
+		}
+		else
+		{
+			GameObject.DontDestroyOnLoad(gameObject);
+			SceneManagerInstance = this;
+		}
+	}
+
+	public IEnumerator LoadScene(string sceneName){
+		if (timeWaited > timeToWaitAroundLoads*2){
+			timeWaited = 0f;
+		}
+		async = Application.LoadLevelAsync(sceneName);
+		async.allowSceneActivation = false;
+
+		yield return async;
+	}
+
+	public void ActivateScene() {
+		async.allowSceneActivation = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!loadingUI){
+			loadingUI = GameObject.Find("LoadingScreen");
+		}
+		if (async != null){
+			timeWaited += Time.deltaTime;
+			Debug.Log(async.progress);
+			Debug.Log(timeWaited);
+			if (async.progress >= 0.9f && timeWaited > timeToWaitAroundLoads){
+				timeWaited = 0f;
+				ActivateScene();
+			}
+		}
+		if (Application.isLoadingLevel){
+			if (loadingUI) {
+				loadingUI.SetActive(true);
+			}
+			else {
+				loadingUI = GameObject.Find("LoadingScreen");
+			}
+
+		}
+		else
+		{
+			if (loadingUI) {
+				loadingUI.SetActive (false);
+			} else {
+				loadingUI = GameObject.Find ("LoadingScreen");
+			}
+
+		}
+	}
+}
