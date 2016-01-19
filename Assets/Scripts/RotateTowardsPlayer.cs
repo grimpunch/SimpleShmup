0afbdae6d376cea386bb00a3ea233a05426200df
Christian Murphy <christheromguy@googlemail.com>
2016-01-19 23:21:00 +0000

    Rotation towards player AI adjusted for multiplayer

--- Assets/Scripts/RotateTowardsPlayer.cs
+++ Assets/Scripts/RotateTowardsPlayer.cs
@@ -1,36 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RotateTowardsPlayer : MonoBehaviour
 {
 
-    public float strength;
-    public Transform target;
+	public float strength;
+	public Transform target;
 
-    // Use this for initialization
-    void Start()
-    {
-        if(target != null && GameObject.Find("ShmupShip")) {
-            target = GameObject.Find("ShmupShip").gameObject.transform;
-        }
-    }
+	// Use this for initialization
+	void Start()
+	{
+		AcquireTarget();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Utils.Paused)
-            return;
-        if(target != null) {
-            //TODO , Dot product calc to find vector of 'down' from this transform to player to determine rotation direction
-            Vector3 vectorToTarget = target.position - transform.position;
-            float angle = (Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg + 90);
-            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward.normalized);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, q, Time.deltaTime * strength);
-        } else {
-            if(!GameObject.Find("ShmupShip")) {
-                return;
-            }
-            target = GameObject.Find("ShmupShip").gameObject.transform;
-        }
-    }
+	void AcquireTarget()
+	{
+		if (!Utils.Multiplayer) {
+			if (GameObject.FindWithTag("Player")) {
+				GameObject prospectiveTarget = GameObject.Find("ShmupShip_P1");
+				if (prospectiveTarget.activeSelf) {
+					target = prospectiveTarget.gameObject.transform;
+				} else {
+					return;
+				}
+			}
+		} else {
+			GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+			if (players == null)
+				return;
+			GameObject prospectiveTarget = players [Random.Range(0, players.Length - 1)].gameObject;
+			if (prospectiveTarget.activeSelf) {
+				target = prospectiveTarget.gameObject.transform;
+			} else {
+				return;
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Utils.Paused)
+			return;
+		if (target != null) {
+			//TODO , Dot product calc to find vector of 'down' from this transform to player to determine rotation direction
+			Vector3 vectorToTarget = target.position - transform.position;
+			float angle = (Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg + 90);
+			Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward.normalized);
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, q, Time.deltaTime * strength);
+		} else {
+			AcquireTarget();
+			return;
+		}
+	}
 }
--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -4,65 +4,68 @@ using System.Collections;
 public class SceneManager : MonoBehaviour
 {
 
-    public static SceneManager SceneManagerInstance;
-    public GameObject loadingUI;
-    private AsyncOperation async;
-    private float timeToWaitAroundLoads = 2f;
-    private float timeWaited;
-    // Use this for initialization
-    void Start()
-    {
-        if(SceneManagerInstance != null) {
-            GameObject.Destroy(gameObject);
-        } else {
-            GameObject.DontDestroyOnLoad(gameObject);
-            SceneManagerInstance = this;
-        }
-    }
+	public static SceneManager SceneManagerInstance;
+	public GameObject loadingUI;
+	private AsyncOperation async;
+	private float timeToWaitAroundLoads = 2f;
+	private float timeWaited;
+	// Use this for initialization
+	void Start()
+	{
+		if (SceneManagerInstance != null) {
+			GameObject.Destroy(gameObject);
+		} else {
+			GameObject.DontDestroyOnLoad(gameObject);
+			SceneManagerInstance = this;
+		}
+	}
 
-    public IEnumerator LoadScene(string sceneName)
-    {
-        if(timeWaited > timeToWaitAroundLoads * 2) {
-            timeWaited = 0f;
-        }
-        async = Application.LoadLevelAsync(sceneName);
-        async.allowSceneActivation = false;
+	public IEnumerator LoadScene(string sceneName)
+	{
+		if (timeWaited > timeToWaitAroundLoads * 2) {
+			timeWaited = 0f;
+		}
+		async = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName);
+		async.allowSceneActivation = false;
 
-        yield return async;
-    }
+		yield return async;
+	}
 
-    public void ActivateScene()
-    {
-        async.allowSceneActivation = true;
-    }
+	public void ActivateScene()
+	{
+		async.allowSceneActivation = true;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(!loadingUI) {
-            loadingUI = GameObject.Find("LoadingScreen");
-        }
-        if(async != null) {
-            timeWaited += Time.deltaTime;
-            if(async.progress >= 0.9f && timeWaited > timeToWaitAroundLoads) {
-                timeWaited = 0f;
-                ActivateScene();
-            }
-        }
-        if(Application.isLoadingLevel) {
-            if(loadingUI) {
-                loadingUI.SetActive(true);
-            } else {
-                loadingUI = GameObject.Find("LoadingScreen");
-            }
+	// Update is called once per frame
+	void Update()
+	{
+		if (!loadingUI) {
+			loadingUI = GameObject.Find("LoadingScreen");
+			GameObject.DontDestroyOnLoad(loadingUI);
+		}
+		if (async != null) {
+			timeWaited += Time.deltaTime;
+			if (async.progress >= 0.9f && timeWaited > timeToWaitAroundLoads) {
+				timeWaited = 0f;
+				ActivateScene();
+			}
+		}
+		if (async == null)
+			return;
+		if (async.progress > 0) {
+			if (loadingUI) {
+				loadingUI.SetActive(true);
+			} else {
+				loadingUI = GameObject.Find("LoadingScreen");
+			}
 
-        } else {
-            if(loadingUI) {
-                loadingUI.SetActive(false);
-            } else {
-                loadingUI = GameObject.Find("LoadingScreen");
-            }
+		} else {
+			if (loadingUI) {
+				loadingUI.SetActive(false);
+			} else {
+				loadingUI = GameObject.Find("LoadingScreen");
+			}
 
-        }
-    }
+		}
+	}
 }
