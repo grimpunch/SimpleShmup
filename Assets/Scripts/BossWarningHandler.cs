3ee48c8c3f444cee1a5c5f7994411243351550a9
Christian Murphy <christheromguy@googlemail.com>
2016-01-21 14:53:58 +0000

    implemented boss warning

---
+++ Assets/Scripts/BossWarningHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossWarningHandler : MonoBehaviour
+{
+
+	public GameObject bossWarn1;
+	public GameObject bossWarn2;
+
+	// Use this for initialization
+	void Start()
+	{
+		bossWarn1 = GameObject.Find("BossWarningText1");
+		bossWarn2 = GameObject.Find("BossWarningText2");
+		bossWarn1.GetComponent<Animator>().enabled = false;
+		bossWarn2.GetComponent<Animator>().enabled = false;
+		this.enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Utils.Paused) {
+			bossWarn1.GetComponent<Animator>().enabled = false;
+			bossWarn2.GetComponent<Animator>().enabled = false;
+		} else {
+			bossWarn1.GetComponent<Animator>().enabled = true;
+			bossWarn2.GetComponent<Animator>().enabled = true;
+		}
+	}
+}
