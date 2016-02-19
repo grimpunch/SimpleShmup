ca6c2a1a7c5ca560a2968ea223b458ffbd0e8fd2
Christian Murphy <christheromguy@googlemail.com>
2016-02-19 13:50:24 +0000

    Scripted opening fixed for 2 players

--- Assets/Scripts/ScriptedOpening.cs
+++ Assets/Scripts/ScriptedOpening.cs
@@ -1,58 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ScriptedOpening : MonoBehaviour
 {
 
-    public GameObject[] explosions;
-    public PlayerMovement playerMoveScript;
-    public ScrollLevelForward scrollScript;
-    public float timeToExplode = 3.0f;
-    public float timeToStartScroll = 4.0f;
-    public float timeToAllowMove = 8f;
-    private float timeAlive;
-    // Use this for initialization
-    void Start()
-    {
-        playerMoveScript.enabled = false;
-        scrollScript.stopped = true;
-    }
+	public GameObject[] explosions;
+	public List<PlayerMovement> playerMoveScripts;
+	public ScrollLevelForward scrollScript;
+	public float timeToExplode = 3.0f;
+	public float timeToStartScroll = 4.0f;
+	public float timeToAllowMove = 8f;
+	private float timeAlive;
+	// Use this for initialization
+	void Start()
+	{
+		foreach (PlayerMovement playerMoveScript in playerMoveScripts) {
+			playerMoveScript.enabled = false;	
+		}
+		scrollScript.stopped = true;
+	}
 
-    void Update()
-    {
-        if(Utils.Paused)
-            return;
+	void Update()
+	{
+		if (Utils.Paused)
+			return;
 
-        timeAlive += Time.deltaTime;
-        if(timeAlive > timeToExplode)
-            EnableExplode();
-        if(timeAlive > timeToStartScroll)
-            EnableScroll();
-        if(timeAlive > timeToAllowMove && playerMoveScript != null)
-            EnableMove();
-        if(timeAlive > (timeToAllowMove + timeToStartScroll + timeToExplode + 2f))
-            DestroyThis();
-    }
+		timeAlive += Time.deltaTime;
+		if (timeAlive > timeToExplode)
+			EnableExplode();
+		if (timeAlive > timeToStartScroll)
+			EnableScroll();
+		if (timeAlive > timeToAllowMove && playerMoveScripts.Count > 0)
+			EnableMove();
+		if (timeAlive > (timeToAllowMove + timeToStartScroll + timeToExplode + 2f))
+			DestroyThis();
+	}
 
-    void EnableExplode()
-    { 
-        foreach(GameObject explosion in explosions) {
-            explosion.SetActive(true);
-        } 
-    }
+	void EnableExplode()
+	{ 
+		foreach (GameObject explosion in explosions) {
+			explosion.SetActive(true);
+		} 
+	}
 
-    void EnableScroll()
-    {
-        scrollScript.stopped = false;
-    }
+	void EnableScroll()
+	{
+		scrollScript.stopped = false;
+	}
 
-    void EnableMove()
-    {
-        playerMoveScript.enabled = true;
-    }
+	void EnableMove()
+	{
+		foreach (PlayerMovement playerMoveScript in playerMoveScripts) {
+			playerMoveScript.enabled = true;
+		}
+	}
 
-    void DestroyThis()
-    {
-        Destroy(this.gameObject);
-    }
+	void DestroyThis()
+	{
+		Destroy(this.gameObject);
+	}
 }
