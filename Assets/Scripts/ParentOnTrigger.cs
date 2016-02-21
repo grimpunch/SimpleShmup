d40ca2af886e17235f7f4f9525dcad9a3c2405ea
Christian Murphy <christheromguy@googlemail.com>
2016-02-21 18:39:12 +0000

    Split prefabs for player ships to avoid re-occuring prefab related issues, tweaked 20sec marker, fixed bug with laser parenting the enemies to the gameplay area ahead of time

--- Assets/Scripts/ParentOnTrigger.cs
+++ Assets/Scripts/ParentOnTrigger.cs
@@ -3,37 +3,39 @@ using System.Collections;
 
 public class ParentOnTrigger : MonoBehaviour
 {
-    public string nameOfObjectToParentTo = "GamePlayArea";
-    public Transform target;
-    public float delay = 0.0f;
-    public bool setLocalPosition = false;
-    public Vector3 localPosition = Vector3.zero;
+	public string nameOfObjectToParentTo = "GamePlayArea";
+	public Transform target;
+	public float delay = 0.0f;
+	public bool setLocalPosition = false;
+	public Vector3 localPosition = Vector3.zero;
 
-    public Transform newParent;
+	public Transform newParent;
 
-    public void Start()
-    {
+	public void Start()
+	{
         
-    }
+	}
 
     
-    void OnTriggerEnter2D(Collider2D col2D)
-    {
-        Invoke("ParentNow", delay);
-    }
+	void OnTriggerEnter2D(Collider2D col2D)
+	{
+		if (col2D.gameObject.layer == LayerMask.NameToLayer("Activator")) {
+			Invoke("ParentNow", delay);
+		}
+	}
 
-    void ParentNow()
-    {
-        if(!newParent)
-            newParent = GameObject.Find(nameOfObjectToParentTo).transform; 
-        if(target == null) {
-            target = transform;
-        } 
-        target.parent = newParent;
+	void ParentNow()
+	{
+		if (!newParent)
+			newParent = GameObject.Find(nameOfObjectToParentTo).transform; 
+		if (target == null) {
+			target = transform;
+		} 
+		target.parent = newParent;
 
-        if(setLocalPosition) {
-            target.localPosition = localPosition;
-        }
-        gameObject.GetComponent<ParentOnTrigger>().enabled = false;
-    }
+		if (setLocalPosition) {
+			target.localPosition = localPosition;
+		}
+		gameObject.GetComponent<ParentOnTrigger>().enabled = false;
+	}
 }
