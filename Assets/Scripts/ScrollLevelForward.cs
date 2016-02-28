4acb540d7f5323415a285c00c7b627ed01d8c6c5
Christian Murphy <christheromguy@googlemail.com>
2016-02-28 16:51:42 +0000

    lvl 1 flow tweaks

--- Assets/Scripts/ScrollLevelForward.cs
+++ Assets/Scripts/ScrollLevelForward.cs
@@ -4,33 +4,33 @@ using System.Collections;
 public class ScrollLevelForward : MonoBehaviour
 {
 
-    public float scrollSpeed = 0.25F;
-    private float movingSpeed;
-    public bool stopped;
-    public float FadeSpeed = 1.0f;
+	public float scrollSpeed = 0.25F;
+	private float movingSpeed;
+	public bool stopped;
+	public float FadeSpeed = 1.0f;
     
-    // Use this for initialization
-    void Start()
-    {
-        movingSpeed = scrollSpeed;
-    }
+	// Use this for initialization
+	void Start()
+	{
+		movingSpeed = scrollSpeed;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Utils.Paused)
-            return;
-        if(!stopped && scrollSpeed <= movingSpeed) {
-            scrollSpeed += Mathf.Lerp(0f, movingSpeed, FadeSpeed * Time.deltaTime);
-        }
-        if(!stopped && scrollSpeed >= movingSpeed - 0.1f)
-            scrollSpeed = movingSpeed;
-        //Slow to a stop
-        if(stopped && scrollSpeed >= 0f) { 
-            scrollSpeed = FadeSpeed * Time.deltaTime;
-            if(scrollSpeed <= 0.1f)
-                scrollSpeed = 0f;
-        }
-        transform.position += transform.up * (scrollSpeed * Time.deltaTime);
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		if (Utils.Paused)
+			return;
+		if (!stopped && scrollSpeed <= movingSpeed) {
+			scrollSpeed += Mathf.Lerp(0f, movingSpeed, FadeSpeed * Time.deltaTime);
+		}
+		if (!stopped && scrollSpeed >= movingSpeed - 0.1f)
+			scrollSpeed = movingSpeed;
+		//Slow to a stop
+		if (stopped && scrollSpeed >= 0f) { 
+			scrollSpeed = FadeSpeed * Time.deltaTime;
+			if (scrollSpeed <= 0.1f)
+				scrollSpeed = 0f;
+		}
+		transform.position += transform.up * (scrollSpeed * Time.deltaTime);
+	}
 }
