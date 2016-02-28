3e97aaeae8f35fb5c0a669f285860c5c050005ad
Christian Murphy <christheromguy@googlemail.com>
2016-02-28 17:10:49 +0000

    reduced rate of fire of captured ships, added time counter for levels

--- Assets/Scripts/FramesPerSecond.cs
+++ Assets/Scripts/FramesPerSecond.cs
@@ -3,26 +3,29 @@ using System.Collections;
 
 public class FramesPerSecond : MonoBehaviour
 {
-    float deltaTime = 0.0f;
+	float deltaTime = 0.0f;
+	float timeSinceStart = 0.0f;
 
-    void Update()
-    {
-        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
-    }
+	void Update()
+	{
+		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+		timeSinceStart = Time.time;
+	}
 
-    void OnGUI()
-    {
-        int w = Screen.width, h = Screen.height;
+	void OnGUI()
+	{
+		int w = Screen.width, h = Screen.height;
 
-        GUIStyle style = new GUIStyle();
+		GUIStyle style = new GUIStyle();
 
-        Rect rect = new Rect(0, 0, w, h * 2 / 100);
-        style.alignment = TextAnchor.UpperLeft;
-        style.fontSize = h * 2 / 100;
-        style.normal.textColor = new Color(0.8f, 0.8f, 0.5f, 1.0f);
-        float msec = deltaTime * 1000.0f;
-        float fps = 1.0f / deltaTime;
-        string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
-        GUI.Label(rect, text, style);
-    }
+		Rect rect = new Rect(0, 0, w, h * 2 / 100);
+		style.alignment = TextAnchor.UpperLeft;
+		style.fontSize = h * 2 / 100;
+		style.normal.textColor = new Color(0.8f, 0.8f, 0.5f, 1.0f);
+		float msec = deltaTime * 1000.0f;
+		float fps = 1.0f / deltaTime;
+		string text = string.Format("{0:0.0} ms ({1:0.} fps)    " + " Time since Start: " + timeSinceStart, msec, fps);
+		GUI.Label(rect, text, style);
+
+	}
 }
