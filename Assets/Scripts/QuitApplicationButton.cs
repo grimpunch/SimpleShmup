b7a6ea1477b3690a7e469c3bc104ade70c185d5d
Christian Murphy <christheromguy@googlemail.com>
2016-03-14 23:48:52 +0000

    Application Can Now Quit!
    There's a quit button and everything

---
+++ Assets/Scripts/QuitApplicationButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class QuitApplicationButton : MonoBehaviour {
+
+    void Start () {
+        Text buttonText = gameObject.GetComponentInChildren<Text>();
+        switch(Application.platform)
+        {
+            case RuntimePlatform.WindowsEditor:
+                buttonText.text = "Quit to Windows";
+                break;
+            case RuntimePlatform.WindowsPlayer:
+                buttonText.text = "Quit to Windows";
+                break;
+            case RuntimePlatform.OSXPlayer:
+                buttonText.text = "Quit to OSX";
+                break;
+            case RuntimePlatform.LinuxPlayer:
+                buttonText.text = "Quit to Linux";
+                break;
+            default:
+                buttonText.text = "Quit";
+                break;
+        }
+	}
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+	
+}
